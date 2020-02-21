f9b6ed4588e53e7dc2b6e0996f0d2d144daa209b
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-02-21 16:16:32 -0600

    Arbors initial home page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class ArborsHomePage extends React.Component{
+  render(){
+       return(  
+            <div style={{
+                       minHeight: '100vh',
+                       position: 'relative',
+                       boxSizing:'border-box',
+                       backgroundColor: 'rgb (255, 255, 255)',
+                      }}
+                     > 
+
+             
+
+                  <div className="content">
+                        <h2>CSS Template</h2>
+                        <p>A topnav, content and a footer.</p>
+                  </div>
+
+                  <div className="footer">
+                        <p>Footer</p>
+                  </div>
+            </div>
+          );
+        }
+}
+ReactDOM.render(<ArborsHomePage />, document.getElementById('root'));
