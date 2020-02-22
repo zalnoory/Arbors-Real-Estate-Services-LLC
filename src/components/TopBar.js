8da2ff6faee867855fec446f07845fd4f7ce1e6e
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-02-22 10:36:41 -0600

    removed inline styling to external css file instead

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,52 +1,23 @@
 import React from "react";
 import logo from './logo/logo.jpg';
+import './TopBar.css';
 
 
 
 
 const TopBar= () => {
-    const TopBarContainerStyle= {
-            boxSizing: 'border-box',
-            borderTop:'1px solid rgba(230, 229, 229, 1)',
-            borderBottom:'1px solid rgba(230, 229, 229, 1)',
-            height:'40px',
-            display:'flex',
-            
-           
-          };
+    return(
+        <div  className= "topBarContainer">
+            <span>
+               <a className="topBarLinks"   href="tel: 000-000-0000"> 
+                   <i className="glyphicon glyphicon-earphone"/>000-000-0000 </a>
+               <a  className="topBarLinks" href="index.js">
+                   <img className="logo" src={logo}  alt="logo" /> </a>         
+            </span>
 
-
-      const TopBarLinks= {
-        display:'inline-block',
-        border:'1px solid black',
-        float:'right',
-        marginRight:'20px',
-        marginLeft:'200px',
-        marginTop:'5px', 
-        textDecoration:'none',
-      }    
-
-    const LogoStyle={
-        width:'100px',
-        height:'30px',
-        border:'0px',
-       
-    };     
-    
-return(
-<div  style= {TopBarContainerStyle}>
-  <span>
-  <a style={TopBarLinks}   href="tel: 000-000-0000"> 
-               <i className="glyphicon glyphicon-earphone"/>000-000-0000 </a>
-  <a  style={TopBarLinks} href="index.js">
-                <img style={LogoStyle} src={logo}  alt="logo" /> </a>         
-    </span>
-
-  </div>
+         </div>
   )
 }
-
-
 export default TopBar;
 
 
