d56b30e940049cace3cfedf75864bf0a7a35fbca
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-02-22 08:54:12 -0600

    Initial Content Component

---
+++ src/components/Content.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+
+const Content= () => {
+
+
+return(
+    <div  className="content-container">
+      <div className= "nav-container" >
+           <ul className="main-nav">
+               <li><a href="#home" class="home">Home</a></li>
+               <li><a href="#home" class="resources">Resources</a></li>
+
+           
+               <li><a href="#contact">Contact</a></li>                
+               <li><a class="active" href="#about">About</a></li>
+           
+       </ul>
+      </div>
+</div>
+  );
+};
+
+
+export default Content; 
