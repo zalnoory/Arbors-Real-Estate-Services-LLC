6d774b907e9d90d2f2745204115175c52cc6c65b
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-02-24 09:27:23 -0600

    added Grid Layout and Media Query

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo/logo.jpg';
-import { TopBarContainer,RowStyle, TopBarLinks, StyledLogo, LinkContainer } from './TopBar.styles';
+import { Grid,Row,Col,TopBarLinks,StyledLogo} from './TopBar.styles';
 
 const TopBar = () => {
   return (
-    <TopBarContainer>
-    <RowStyle class="row">
-         <LinkContainer class="column">
-              <TopBarLinks href="index.js">
-                    <StyledLogo className="logo" src={logo} alt="logo" />{' '}
-              </TopBarLinks>
-         </LinkContainer>
+    <Grid>
+      <Row>
+         <Col size={1} collapse= "xs">
+            
+                 <TopBarLinks href="index.js">
+                     <StyledLogo className="logo" src={logo} alt="logo" />{' '}
+                  </TopBarLinks>
+             
+         </Col>
 
-         <LinkContainer class="column">
-              <TopBarLinks href="tel: 000-000-0000">
-                     <i className="glyphicon glyphicon-earphone" />
+         <Col size={1}>
+              
+                   <TopBarLinks href="tel: 000-000-0000">
+                      <i className="glyphicon glyphicon-earphone" />
                                  000-000-0000{' '}
-              </TopBarLinks>
+                    </TopBarLinks>
+          
         
-        </LinkContainer>
-        </RowStyle>
-    </TopBarContainer>
+          </Col>
+      </Row>
+    </Grid>
   )
 }
 export default TopBar
--- src/components/TopBar.styles.js
+++ src/components/TopBar.styles.js
@@ -1,28 +1,39 @@
 import styled from 'styled-components'
 
-export const TopBarContainer = styled.div`
+export const Grid = styled.div`
   box-sizing: border-box;
   border-top: 1px solid rgba(230, 229, 229, 1);
   border-bottom: 1px solid rgba(230, 229, 229, 1);
   height:40px;
-  display: flex;
-`
+`;
 
-export const RowStyle= styled.div`
-.row {
-  display: flex;
+export const Row= styled.div`
+display: flex;
+`;
+
+
+const media={
+  /* Extra small devices (phones, 600px and down) */
+  xs:(styles)=> `
+  @media only screen and (max-width: 600px) {
+    ${styles}
+  }
+  `,
 }
 
-`
 
+export const Col= styled.div`
 
-export const LinkContainer= styled.div`
-box-sizing: border-box;
-display:flex;
-width:50%;
+flex:${(props)=> props.size};
+
+${(props)=> props.collapse && media[props.collapse](`
+display: none;
+`)};
+
 border:1px solid red;
+height:35px;
 
-`
+`;
 
 
 export const TopBarLinks = styled.a`
@@ -32,10 +43,10 @@ export const TopBarLinks = styled.a`
   margin-left: 200px;
   margin-top: 5px;
   text-decoration: none;
-`
+`;
 
 export const StyledLogo = styled.img`
   width: 100px;
   height: 30px;
   border: 0px;
-`
+`;
