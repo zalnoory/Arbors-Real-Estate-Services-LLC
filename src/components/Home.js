6dd9162fd5c287aac7fc717c772d4be0caa9747d
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-03-03 21:46:30 -0600

    modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Container,ChildContainer,Form, } from './NavBar.styles';
 import NavBar from './NavBar.js';
-// import img from './photo-1556955112-28cde3817b0a.jpeg';
+
 class Home extends React.Component{
     
     
