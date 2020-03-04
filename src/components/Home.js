3ecd604562a9c256b0df7231bcabb28842f70092
Zahrah Alnoory <zahrahalnoory@gmail.com>
2020-03-03 21:40:36 -0600

    added styling for Form Element

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import {Container,ChildContainer,Form, } from './NavBar.styles';
+import NavBar from './NavBar.js';
+// import img from './photo-1556955112-28cde3817b0a.jpeg';
+class Home extends React.Component{
+    
+    
+    
+    render(){
+        return(
+      <Container>
+       <ChildContainer>
+       <Form>
+      
+    <p style={{textAlign:'center'}}>FIND A HOME</p>
+     <input type="text"/><input type="text"/><input type="text"/><input type="text"/>
+       </Form>
+       </ChildContainer>
+       </Container>
 
+        )
+    }}
 
-
-const Home= ()=> {
-    return(
-    <h2>Home</h2>
-    ) ;
-
-   }
-
+ 
+    
 export default Home;
